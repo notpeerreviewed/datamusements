acfa5032a5b5b06279650b241c87b12c1af24cbc
notpeerreviewed <jrl311275@gmail.com>
2022-02-20 17:49:25 +1300

    Amending folder structure

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,16 @@ module.exports = function(eleventyConfig){
 
     eleventyConfig.addPassthroughCopy('assets')
 
+    return {
+
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dir: {
+            input: 'src',
+            output: '_site'
+        }
+
+    }
+
 }
